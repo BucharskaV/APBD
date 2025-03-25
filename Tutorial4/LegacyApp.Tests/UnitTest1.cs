24c1b9c4c1095eb5a7bc7547d4926fc7fce83842
Vladyslava s30853 <s30853@pjwstk.edu.pl>
2025-03-25 11:24:58 +0100

    Tutorial4 Start

---
+++ Tutorial4/LegacyApp.Tests/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace LegacyApp.Tests
+{
+    public class UnitTest1
+    {
+        private Calculator _sut;
+
+        public UnitTest1()
+        {
+            _sut = new Calculator();
+        }
+        
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        
+        [Theory]
+        [InlineData(1, 2)]
+        [InlineData(-2, -3)]
+        public void Test2(int a, int b)
+        {
+            
+        }
+    }
+}
