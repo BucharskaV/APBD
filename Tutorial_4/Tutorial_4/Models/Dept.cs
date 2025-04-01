6b3650abef11aafc0d506bf18f93b505b00aa0d0
Vladyslava s30853 <s30853@pjwstk.edu.pl>
2025-04-01 10:56:29 +0200

    Initial Solution

---
+++ Tutorial_4/Tutorial_4/Models/Dept.cs
@@ -0,0 +1,8 @@
+namespace Tutorial_4.Models;
+
+public class Dept
+{
+    public int DeptNo { get; set; }
+    public string DName { get; set; }
+    public string Loc { get; set; }
+}
---
+++ Tutorial_4/Tutorial_4/Program.cs
@@ -0,0 +1,9 @@
+namespace Tutorial_4;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+    }
+}
