8e5da5b449a45f678c6d9829bb22d6c95b8027ef
Vladyslava Bucharska - <s30853@pjwstk.edu.pl>
2025-04-08 11:33:16 +0200

    Delete Tutorial5RestApi - Start directory

--- Tutorial5RestApi - Start/Users.Api/Models/Order.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Users.Api.Models;
-
-public class Order
-{
-    public int Id { get; set; }
-    public int UserId { get; set; }
-    public Dictionary<int, string> Products { get; set; } = [];
-}
--- Tutorial5RestApi - Start/Users.Api/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Users.Api.Models;
-
-public class User
-{
-    public required int Id { get; set; }
-    public required string FullName { get; set; }
-    public required string Email { get; set; }
-}
